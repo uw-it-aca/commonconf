2755ccfa24b5e1114a62ecaad9ba9867d5438d26
Patrick Michaud <pmichaud@uw.edu>
2017-02-23 12:02:38 -0800

    Trying to hookup pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
 
-setup(name='CommonConf',
-      version='0.1',
-      description='',
-      install_requires=[],
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/commonconf>`_.
+"""
+
+# The VERSION file is created by travis-ci, based on the tag name
+version_path = 'commonconf/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+url = "https://github.com/uw-it-aca/commonconf"
+setup(
+    name='CommonConf',
+    version=VERSION,
+    packages=['commonconf'],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[],
+    license='Apache License, Version 2.0',
+    description=('An interface for getting settings from a variety'
+                 'of configuration libraries.'),
+    long_description=README,
+    url=url,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
