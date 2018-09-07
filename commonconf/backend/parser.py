b4f2c4de0c67bb61ca8b9d01ad3759439c45fde5
Fang Lin <fanglin@uw.edu>
2018-09-07 10:36:48 -0700

    fix import, except, format

--- commonconf/backend/parser.py
+++ commonconf/backend/parser.py
@@ -1,18 +1,15 @@
-try:
-    import ConfigParser
-except:
-    import configparser as ConfigParser
+from configparser import RawConfigParser, NoOptionError
 
 
 class ConfigParserBackend(object):
     def __init__(self, path, section):
-        self.config = config = ConfigParser.RawConfigParser()
+        self.config = config = RawConfigParser()
         self.config.read(path)
         self.section = section
 
     def get(self, key):
         try:
             return self.config.get(self.section, key)
-        except ConfigParser.NoOptionError as ex:
-            raise AttributeError("Key %s not in section %s" % (key,
-                                                               self.section))
+        except NoOptionError as ex:
+            raise AttributeError(
+                "Key {} not in section {}".format(key, self.section))
